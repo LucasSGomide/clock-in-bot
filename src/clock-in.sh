8d96562d6ca4d8ebd2935b957865707035d8420e
Lucas Gomide <lucas.sgomide92@gmail.com>
2024-10-31 13:08:31 -0300

    wip: login into new platform

--- src/clock-in.sh
+++
@@ -1,5 +0,0 @@
-path=$(pwd)
-# filename=$(basename "$path")
-
-echo "${path}" > path.txt
-# node ./Dev/freelancer/clock-in-bot/src/index.js
--- src/index.js
+++ src/index.js
@@ -13,30 +13,24 @@ async function runClockIn() {
   await page.goto(process.env.LOGIN_URL);
 
   await page.evaluate(() => {
-    localStorage.setItem("modalAlreadyDisplayed", "true");
+    localStorage.setItem("terms@lastVerify", new Date().toISOString());
   });
 
-  const loginInput = await page.waitForSelector(
-    '[placeholder="Enter your user"]'
-  );
+  const loginInput = await page.waitForSelector('#login input#matricula');
   await loginInput.type(process.env.USER_LOGIN);
 
-  const passwordInput = await page.waitForSelector(
-    '[placeholder="Enter your password"]'
-  );
+  const passwordInput = await page.waitForSelector('[placeholder="Senha"]');
   await passwordInput.type(process.env.USER_PASSWORD);
 
-  await page.click(
-    'button[type="button"].po-button.po-text-ellipsis.po-button-primary'
-  );
+  await page.click('#boxLogin button.btn.btn-primary.pull-right');
 
-  await page.waitForSelector('li p[aria-label="Menu Attendance"]');
-  await page.goto(process.env.CLOCK_IN_URL);
+  // await page.waitForSelector('li p[aria-label="Menu Attendance"]');
+  // await page.goto(process.env.CLOCK_IN_URL);
 
-  const swipeButton = await page.waitForSelector("#div-swipeButtonText");
-  await swipeButton.click({ clickCount: 2 });
+  // const swipeButton = await page.waitForSelector("#div-swipeButtonText");
+  // await swipeButton.click({ clickCount: 2 });
 
-  await page.waitForSelector("#div-swipeButtonOperationSuccessText");
+  // await page.waitForSelector("#div-swipeButtonOperationSuccessText");
   // await browser.close()
 }
 
